d3be231793f75b60c42e1fb0c2430ac9d32a70c5
Mary-Samuela <samuelamary@gmail.com>
2026-02-23 18:23:52 +0300

    updated movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const MovieCard = ({ title, description, posterURL, rating }) => {
+const MovieCard = ({ title, posterURL, movieId }) => {
   return (
-    <div className="border rounded p-4 w-72 m-2 shadow-lg">
-      <img
-        src={posterURL}
-        alt={title}
-        className="w-full h-64 object-cover rounded"
-      />
-      <h2 className="text-xl font-bold mt-2">{title}</h2>
-      <p>{description}</p>
-      <p className="font-semibold mt-1">Rating: {rating}/5</p>
-    </div>
+    <Link to={`/movie/${movieId}`}>
+      <div className="border rounded p-4 w-72 m-2 shadow-lg cursor-pointer hover:scale-105 transition-transform">
+        <img
+          src={posterURL}
+          alt={title}
+          className="w-full h-64 object-cover rounded"
+        />
+        <h2 className="text-xl font-bold mt-2">{title}</h2>
+      </div>
+    </Link>
   );
 };
 
